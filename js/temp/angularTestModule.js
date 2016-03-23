e4ca59b1e74bc5ff781476b45a16542fd79f7c34
huxiangtao <elliot130@163.com>
2016-03-23 18:00:45 +0800

    add AngularDateTest

--- js/temp/angularTestModule.js
+++ js/temp/angularTestModule.js
@@ -3,8 +3,24 @@
  */
 
 
-    angular.module('angularTestModule',[])
-        .controller('angularTestCtrl',function($scope) {
-            $scope.name = "elliot"
-            $scope.age = "18"
-        })
+angular.module('angularTestModule',[])
+    .controller('angularTestCtrl',function($scope) {
+        $scope.name = "elliot"
+        $scope.age = "18"
+        $scope.change = function() {
+            alert("window")
+        }
+    })
+    .directive('test',function() {
+        return {
+            template:'<div>yes</div>',
+            replace:true,
+            link: function(scope) {
+                scope.beforerender()
+            },
+            scope: {
+                beforerender:'&'
+            }
+
+        }
+    })
