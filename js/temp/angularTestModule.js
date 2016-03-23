bf15cb46f10dba7217615260e4f676bcedd8ccee
huxiangtao <elliot130@163.com>
2016-03-23 17:49:36 +0800

    add AngularDateTest

---
+++ js/temp/angularTestModule.js
@@ -0,0 +1,10 @@
+/**
+ * Created by huxiangtao on 2016/3/23.
+ */
+
+
+    angular.module('angularTestModule',[])
+        .controller('angularTestCtrl',function($scope) {
+            $scope.name = "elliot"
+            $scope.age = "18"
+        })
